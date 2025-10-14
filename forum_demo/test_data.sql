7dcd8a1e6225699bb686944fdd13a9b7844d8140
VoxelMagpie <150785799+voxelmagpie@users.noreply.github.com>
2025-10-14 13:32:12 +0100

    Forum demo
    Added database access (read-only), HTML generation, and CSS.

--- forum_demo/test_data.sql
+++ forum_demo/test_data.sql
@@ -1,3 +1,7 @@
 USE forum;
 
-INSERT INTO Posts (text) VALUES ("Hello")
+DELETE FROM Posts;
+
+INSERT INTO Posts (text) VALUES ("Maecenas ornare, tellus nec elementum ullamcorper, odio arcu consectetur mi, quis convallis ante metus id lectus.");
+INSERT INTO Posts (text) VALUES ("Proin nec aliquam massa. Suspendisse et ante vitae ante rutrum varius quis vitae massa.");
+INSERT INTO Posts (text) VALUES ("Etiam dictum dui ac mi fermentum elementum.");
